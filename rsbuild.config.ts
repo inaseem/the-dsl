7ba5fd66ef7384bc75df60dad01d119f531be858
inaseem <inaseem@protonmail.com>
2024-06-23 11:09:52 +0530

    chore: fix ci

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 import { pluginStyledComponents } from '@rsbuild/plugin-styled-components';
+const mode = process.env.NODE_ENV || 'development';
+import { name } from './package.json';
 
 export default defineConfig({
   plugins: [pluginReact(), pluginStyledComponents()],
   html: {
     template: 'public/index.html',
     mountId: 'root',
+  },
+  output: {
+    assetPrefix: mode === 'production' ? `/${name}/` : '/',
   },
   tools: {
     rspack: {
